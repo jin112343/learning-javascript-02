ce75ca9c92bc8ea587eb91a311ac567bfca098ff
mizoijin <mizoijin@icloud.com>
2019-06-28 14:28:58 +0900

    6/28 課題

--- 01.js
+++ 01.js
@@ -9,6 +9,13 @@ process.stdout.write("Buzz");
 console.log();
 
 
-for (let i = 0; i < 15; i++) {
-    console.log(i)
-}
+    for (let i = 1; i < 16; i++) 
+      if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+      } else if (i % 3 == 0) {
+        console.log("Fizz");
+      } else if (i % 5 == 0) {
+        console.log("Buzz");
+      } else {
+        console.log(i);
+      }
