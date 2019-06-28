11970539b8c67025db890b1e70a4183796654934
mizoijin <mizoijin@icloud.com>
2019-06-28 13:04:49 +0900

    6/28 課題

--- 01.js
+++ 01.js
@@ -2,9 +2,13 @@
 
 // サンプル: "1" という文字列を出力
 console.log("1");
-
 // サンプル: "FizzBuzz" という文字列を出力
 // (process.stdout.write で改行なしの出力ができる)
 process.stdout.write("Fizz");
 process.stdout.write("Buzz");
 console.log();
+
+
+for (let i = 0; i < 15; i++) {
+    console.log(i)
+}
